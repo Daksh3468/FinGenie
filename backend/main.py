7c5cb1b2b9fbdd66386f5a2f529ba993f912dbbf
RushitSoni <115700646+RushitSoni@users.noreply.github.com>
2026-04-26 10:44:41 +0530

    CORS changes

--- backend/main.py
+++ backend/main.py
@@ -24,14 +24,26 @@ app = FastAPI(
 )
 
 # CORS — read from environment
-ALLOWED_ORIGINS = os.getenv(
-    "ALLOWED_ORIGINS",
-    "http://localhost:5173,http://127.0.0.1:5173"
-).split(",")
+# ALLOWED_ORIGINS = os.getenv(
+#     "ALLOWED_ORIGINS",
+#     "http://localhost:5173,http://127.0.0.1:5173"
+# ).split(",")
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://fingenie-frontend-bebq.onrender.com",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
